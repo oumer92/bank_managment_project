8985971d58f2c1913d874ffd8ab3681e6d65eaf3
Oumer <101581075+oumer92@users.noreply.github.com>
2022-08-05 14:07:08 +0300

    Update bank_record.cpp

--- bank_record.cpp
+++ bank_record.cpp
@@ -1,1 +1,13 @@
 
+
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string.h>
+#include <iomanip>
+#include <cstdio>
+#include <conio.h>
+#include <ctime>
+#include <stdlib.h>
+#include <bits/stdc++.h>
